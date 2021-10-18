b6fd818b188d6061bedbb9aaa3b6d612a7f4b433
Brady Buchholz <bradybuchholz@gmail.com>
2021-10-18 13:08:58 -0400

    Basic files created

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $("#currentDay").text(today.format("MMM Do, YYYY"));
 var currentTime = moment().format('H');
 
 var allRows = document.querySelectorAll(".row")
-// console.log(allRows)
+
 for (var i = 0; i < allRows.length; i++) {
     var timeBlockHour = parseInt(allRows[i].id)
     // console.log(allRows[i].id) 
@@ -18,11 +18,16 @@ for (var i = 0; i < allRows.length; i++) {
         allRows[i].classList.add("present")
     }
 }
-//todo project
-// Use window.localStorage to retrieve and store your data object as string
-const LS = JSON.parse(localStorage.scheduler || '{}'); // now an Object
+var saveButton = document.querySelectorAll(".Button")
 
-//click makes calendar content editable
+document.addEventListener("click", saveButton)
+for (var i = 0; i < saveButton.length; i++){
+    var saveButton
+}
+// Use window.localStorage to retrieve and store your data object as string
+// const LS = JSON.parse(localStorage.scheduler || '{}'); // now an Object
+
+// //click makes calendar content editable
 
 $(".textData").click(function () {
     $(this).addClass('d-none')
@@ -38,35 +43,3 @@ $(".btn").click(function () {
 
     var timeInputId = time + "Input";
 });
-
-
-
-
-
-// make background color of / past hours gray / current hour red / future hours green
-var hourPast = hourPast < currentTime;
-var hourNow = currentTime;
-var hourFuture = hourFuture < currentTime;
-// saves user input
-function renderLastRegistered() {
-    var userInput = localStorage.getItem("event");
-    userEventSpan.textContent = userInput;
-}
-localStorage.setItem("userInput", userInput);
-renderLastRegistered();
-
-var hourNow = currentTime;
-console.log(hourNow)
-console.log(currentTime)
-function backgroundColorEdit() {
-    if (hourNow == currentTime) {
-        document.getElementsByClassName("backgroundTool").style.backgroundColor = "#ff0000";
-    } else if (hourNow < currentTime) {
-        document.getElementsByClassName("backgroundTool").style.backgroundColor = "#d3d3d3";
-    } else {
-        document.getElementsByClassName("backgroundTool").style.backgroundColor = "#90ee90";
-    }
-}
-// if currentTime = hourNow make background color Red //
-
-// if currentTime < hourNow make background color green //
