f07a37f79b08a47c80f58ad3c3d1016e52a07a89
Brady Buchholz <bradybuchholz@gmail.com>
2022-04-18 22:41:35 -0400

    colors change based on if time block is in future, past, or present

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
-var userEventSpan = document.querySelector(".textData");
+const writeButton = document.getElementsByClassName('.Button');
+// writeButton.addEventListener('click', writeSchedule);
+var userEventSpan = document.querySelector('.textData');
 var today = moment();
-$("#currentDay").text(today.format("MMM Do, YYYY"));
+$('#currentDay').text(today.format('MMM Do, YYYY'));
 
 //currentTime to determine if the hour matchs for color
 var currentTime = moment().format('H');
+console.log(currentTime);
 
-var allRows = document.querySelectorAll(".row")
+var allRows = document.querySelectorAll('.row');
 
 for (var i = 0; i < allRows.length; i++) {
-    var timeBlockHour = parseInt(allRows[i].id)
-    // console.log(allRows[i].id) 
-    if (timeBlockHour < currentTime) {
-        allRows[i].classList.add("past")
-    } else if (timeBlockHour > currentTime){
-        allRows[i].classList.add("future")
-    } else {
-        allRows[i].classList.add("present")
-    }
+  var timeBlockHour = parseInt(allRows[i].id);
+  console.log(allRows[i].id);
+  if (timeBlockHour < currentTime) {
+    $(allRows[i]).css('background-color', 'white');
+  } else if (timeBlockHour > currentTime) {
+    $(allRows[i]).css('background-color', 'green');
+  } else {
+    $(allRows[i]).css('background-color', 'red');
+  }
 }
-var saveButton = document.querySelectorAll(".Button")
 
-document.addEventListener("click", saveButton)
-for (var i = 0; i < saveButton.length; i++){
-    var saveButton
-}
-// Use window.localStorage to retrieve and store your data object as string
-// const LS = JSON.parse(localStorage.scheduler || '{}'); // now an Object
+// document.addEventListener('click', saveButton);
+// for (var i = 0; i < saveButton.length; i++) {
+//   var saveButton;
+// }
 
 // //click makes calendar content editable
 
-$(".textData").click(function () {
-    $(this).addClass('d-none')
-    $(this).siblings("input").removeClass("d-none");
-});
+function writeSchedule() {
+  document.getElementsByClassName('textData').textData = '';
+  console.log('write button');
+}
 
-$(".btn").click(function () {
-    console.log(this);
-    console.log($(this).attr("id"))
-    var id = $(this).attr("id")
-    var time = id.slice(0, 3)
-    console.log(time)
+// $('.textData').click(function () {
+//   $(this).addClass('d-none');
+//   $(this).siblings('input').removeClass('d-none');
+// });
 
-    var timeInputId = time + "Input";
-});
+// $('.btn').click(function () {
+//   console.log(this);
+//   console.log($(this).attr('id'));
+//   var id = $(this).attr('id');
+//   var time = id.slice(0, 3);
+//   console.log(time);
+
+//   var timeInputId = time + 'Input';
+// });
