6e46b26d5c0c05daabc6f122b99f709c5aadd25c
Brady Buchholz <bradybuchholz@gmail.com>
2022-04-21 15:03:45 -0400

    saving task for 9am block works

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const writeButton = document.getElementsByClassName('.Button');
-// writeButton.addEventListener('click', writeSchedule);
-var userEventSpan = document.querySelector('.textData');
 var today = moment();
 $('#currentDay').text(today.format('MMM Do, YYYY'));
 
+const task9 = document.getElementById('span9');
+task9.value = localStorage.getItem('task9');
+console.log(task9.value);
+task9.innerText = task9.value;
+
 //currentTime to determine if the hour matchs for color
 var currentTime = moment().format('H');
-console.log(currentTime);
 
 var allRows = document.querySelectorAll('.row');
 
 for (var i = 0; i < allRows.length; i++) {
   var timeBlockHour = parseInt(allRows[i].id);
-  console.log(allRows[i].id);
   if (timeBlockHour < currentTime) {
     $(allRows[i]).css('background-color', 'white');
   } else if (timeBlockHour > currentTime) {
@@ -25,4 +25,9 @@ for (var i = 0; i < allRows.length; i++) {
 $('.textData').click(function () {
   $(this).addClass('d-none');
   $(this).siblings('input').removeClass('d-none');
+});
+
+$('#save9').click(function () {
+  let task = document.getElementById('input9').value;
+  localStorage.setItem('task9', task);
 });
